b931ec151025959e51c0fac4d8300d809f01fde0
Carina Meireles <carinacmeireles@gmail.com>
2024-01-23 18:39:36 +0000

    add meta properties

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,12 @@
 <template>
+  <head>
+    <meta property="og:title" content="Password Generator App" />
+    <meta
+      property="og:description"
+      content="Generate a new password with a series of selectable requirements"
+    />
+    <meta property="og:image" content="/src/assets/PasswordGenerator.png" />
+  </head>
   <main id="password-app">
     <div>
       <h1>Password Generator</h1>
